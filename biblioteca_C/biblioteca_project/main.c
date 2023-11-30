c41e3a8db63e0e5e308081ea373e3bd763bd3698
Eduardo Henrique <educoloni12@gmail.com>
2023-11-30 20:27:23 -0300

    Introduzido a funcionalidade de deletar livros e mais alguns tratamentos de strings

--- biblioteca_C/biblioteca_project/main.c
+++ biblioteca_C/biblioteca_project/main.c
@@ -4,7 +4,6 @@
 #define MAX_LIVROS 50
 
 typedef struct {
-    int codigo;
     char titulo[50];
     char autor[50];
     int emprestado;
@@ -14,8 +13,10 @@ typedef struct {
 Livro livros[MAX_LIVROS];
 
 void menu();
-void cadastrar();
+void cadastrarLivros();
 void listarLivros();
+void deletarLivros();
+void listarLivrosDelet();
 
 int main(){
 
@@ -27,7 +28,6 @@ void menu(){
     int op;
     do {
         system("clear"); //Esse para quem estiver no linux
-        //system(cls) Para quem estiver no windows
         printf("\n1 - Cadastrar Livro \n2 - Emprestar Livro \n3 - Devolver Livro ");
         printf("\n4 - Listar Livros \n5 - Excluir Livro \n0 - Sair\n");
         printf("Selecione uma opção: ");
@@ -36,30 +36,40 @@ void menu(){
 
         switch (op) {
             case 1:
-                cadastrar();
+                cadastrarLivros();
                 break;
             case 4:
                 listarLivros();
+                break;
+            case 5:
+                deletarLivros();
                 break;
         }
 
     } while (op != 0);
 }
 
-void cadastrar(){
+void cadastrarLivros(){
     char titulo[50];
     char autor[50];
     int op;
     do {
         system("clear"); //Esse para quem estiver no linux
-        //system(cls) Para quem estiver no windows
         printf("\nTitulo: ");
         fgets(titulo, sizeof(titulo), stdin);
         titulo[strcspn(titulo, "\n")] = '\0';  // Remover o caractere de nova linha
+        if (titulo[0] == '\0'){
+            printf("O campo titulo não pode estar vazio ou conter espaço no seu inicio, voltando a tela inicial");
+            return;
+        }
 
         printf("\nAutor: ");
         fgets(autor, sizeof(autor), stdin);
         autor[strcspn(autor, "\n")] = '\0';  // Remover o caractere de nova linha
+        if (autor[0] == '\0'){
+            printf("O campo autor não pode estar vazio ou conter espaço no seu inicio, voltando a tela inicial");
+            return;
+        }
 
         printf("\n1 - Continuar\n0 - Sair\n");
         scanf("%d", &op);
@@ -81,14 +91,38 @@ void cadastrar(){
 
 void listarLivros(){
     system("clear"); //Esse para quem estiver no linux
-    //system(cls) Para quem estiver no windows
     printf("\n LISTA DE LIVROS\n");
     for (int i = 0; i < MAX_LIVROS; ++i) {
         if(livros[i].ativo==1){
+            printf("Código: %d\n", i+1);
             printf("Titulo: %s\n", livros[i].titulo);
             printf("Autor: %s\n", livros[i].autor);
             printf("\n-----------------------\n");
         }
     }
+    printf("Pressione enter para voltar");
     getchar(); // Limpar o buffer de entrada
+}
+
+void listarLivrosDelet(){
+    printf("\n LISTA DE LIVROS\n");
+    for (int i = 0; i < MAX_LIVROS; ++i) {
+        if(livros[i].ativo==1){
+            printf("Código: %d\n", i+1);
+            printf("Titulo: %s\n", livros[i].titulo);
+            printf("Autor: %s\n", livros[i].autor);
+            printf("\n-----------------------\n");
+        }
+    }
+}
+
+void deletarLivros(){
+    int codigo;
+    listarLivrosDelet();
+    printf("\n Qual o codigo do livro a ser removido: \n");
+    scanf("%d", &codigo);
+    printf("Livro de codigo: %d excluido com sucesso\n", codigo);
+    --codigo;
+    livros[codigo].ativo=0;
+    getchar();
 }
